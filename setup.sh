75fe10107efc7348dd6fea2003ec78caead82e3f
AbdulInvoke <abdulhafiiz@invokeisdata.com>
2024-01-03 17:15:43 +0800

    updated setup script

--- setup.sh
+++ setup.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-sqlite3 ./database/website_database.db < ./database/Database_Schema.sql
+# https://stackoverflow.com/questions/20095351/shebang-use-interpreter-relative-to-the-script-path
+# https://unix.stackexchange.com/questions/17499/get-path-of-current-script-when-executed-through-a-symlink
+
+
+# sqlite3 ./database/website_database.db < ./database/setup/setup_database.sql
+# sqlite3 ./database/website_database.db < ./database/setup/import_data_p1.sql
+./database/setup/import_data_p2.py
+# sqlite3 ./database/website_database.db < ./database/setup/import_data_p3.sql
