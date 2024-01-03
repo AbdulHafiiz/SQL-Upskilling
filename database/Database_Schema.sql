c3ccac7f6d707cd5fdbb89e36e9c3948220d6179
AbdulInvoke <abdulhafiiz@invokeisdata.com>
2024-01-03 10:02:14 +0800

    added in special error values for handling missing permissions, status and users

--- database/Database_Schema.sql
+++ database/Database_Schema.sql
@@ -151,6 +151,15 @@ COMMIT;
 
 BEGIN;
 
+INSERT INTO status_reference (status_id, "name", category)
+VALUES (-1, 'missing', 'error_handling');
+
+INSERT INTO permission_id (permission_id, "name")
+VALUES (-1, 'missing');
+
+INSERT INTO users_table (user_id, username, creation_timestamp, status_id, permission_id)
+VALUES (-1, 'missing', 0, -1, -1);
+
 .import --csv --skip 1 ./database/csv/comments_table.csv comments_table
 .import --csv --skip 1 ./database/images_table.csv images_table
 .import --csv --skip 1 ./database/permission_reference.csv permission_reference
